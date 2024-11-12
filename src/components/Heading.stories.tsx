33b3d223f85014c30597772b7e97a4c9f65eee8a
Angelica Alves Willrich <angelica@willri.ch>
2024-11-12 10:17:50 +0100

    update dependencies

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,54 +1,57 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { Heading, HeadingProps } from './Heading';
+import { Meta, StoryObj } from "@storybook/react";
+import { Heading, HeadingProps } from "./Heading";
 
 export default {
-    title: 'Components/Heading',
-    component: Heading,
-    args: {
-        children: 'Lorem ipsum',
-        size: 'md'
+  title: "Components/Heading",
+  component: Heading,
+  args: {
+    children: "Lorem ipsum",
+    size: "md",
+  },
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
     },
-    argTypes: {
-        size: {
-            options: ['sm', 'md', 'lg'],
-            control: {
-                type: 'inline-radio'
-            }
-        }
-    }
-} as Meta<HeadingProps>
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<HeadingProps>;
 
-export const Default: StoryObj<HeadingProps> = {}
+export const Default: StoryObj<HeadingProps> = {};
 
 export const Small: StoryObj<HeadingProps> = {
-    args: {
-        size: 'sm'
-    }
-}
+  args: {
+    size: "sm",
+  },
+};
 
 export const Large: StoryObj<HeadingProps> = {
-    args: {
-        size: 'lg'
-    }
-}
+  args: {
+    size: "lg",
+  },
+};
 
 export const CustomComponent: StoryObj<HeadingProps> = {
-    args: {
-        asChild: true,
-        children: (
-            <h1>Heading with h1</h1>
-        )
+  args: {
+    asChild: true,
+    children: <h1>Heading with h1</h1>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
     },
-    argTypes: {
-        children: {
-            table: {
-                disable: true,
-            }
-        },
-        asChild: {
-            table: {
-                disable: true
-            }
-        }
-    }
-}
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
